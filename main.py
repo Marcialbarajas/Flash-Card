dfe616b03cb42e2873babf01143670d33a153c3a
Marcial <marcialbarajas@gmail.com>
2022-07-25 12:56:08 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,75 @@
+from tkinter import *
+import pandas as pd
+import random
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+# ------------------------------------------- Pandas ------------------------------------
+
+try:
+    df = pd.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pd.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = df.to_dict(orient="records")
+# print(to_learn)
+
+current_card = {}
+
+
+def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
+    current_card = random.choice(to_learn)
+    canvas.itemconfig(card_title, text="French")
+    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_background, image=front_img)
+    flip_timer = window.after(3000, func=flip_card)
+
+
+def flip_card():
+    canvas.itemconfig(card_title, text="English")
+    canvas.itemconfig(card_word, text=current_card["English"])
+    canvas.itemconfig(card_background, image=back_img)
+
+
+def is_known():
+    to_learn.remove(current_card)
+    # print(len(to_learn))
+    df = pd.DataFrame(to_learn)
+    df.to_csv("data/words_to_learn.csv", index=False)
+    next_card()
+
+
+# ----------------------------------------Interface-----------------------------------------------
+
+
+window = Tk()
+
+window.title("Flash card App")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+flip_timer = window.after(3000, func=flip_card)
+
+canvas = Canvas(height=526, width=800)
+
+front_img = PhotoImage(file="images/card_front.png")
+back_img = PhotoImage(file="images/card_back.png")
+card_background = canvas.create_image(400, 263, image=front_img)
+canvas.grid(row=0, column=0, columnspan=2)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+card_title = canvas.create_text(400, 150, text="French", font=("Calibri", 40, "italic"))
+card_word = canvas.create_text(400, 263, text="Potato", font=("Calibri", 60, "bold"))
+
+wrong_image = PhotoImage(file="images/wrong.png")
+right_image = PhotoImage(file="images/right.png")
+
+button_right = Button(image=right_image, highlightthickness=0, command=next_card)
+button_right.grid(row=1, column=0)
+button_wrong = Button(image=wrong_image, highlightthickness=0, command=is_known)
+button_wrong.grid(row=1, column=1)
+
+next_card()
+
+window.mainloop()
